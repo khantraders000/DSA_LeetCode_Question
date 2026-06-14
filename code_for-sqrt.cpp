b6391b1f5201b4b867b1a5c86d7dc7473cd7e2ed
Ayan Khan <ayan180013@gmail.com>
2026-06-14 23:39:53 +0530

    all bugs fixed code

--- code_for-sqrt.cpp
+++ code_for-sqrt.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int sqrt(int n)
 {
+    // O(logn)
+    int ans = -1;
     int s = 0;
     int e = n;
     int target = n;
@@ -19,7 +21,7 @@ int sqrt(int n)
         }
         else
         {
-            int ans = mid;
+            ans = mid;
             s = mid + 1;
         }
         mid = s + (e - s) / 2;
@@ -27,13 +29,39 @@ int sqrt(int n)
     return ans;
 }
 
+double precisionSQRT(int n){
+    // O(precision)
+    double square = sqrt(n);
+    int precision = 12;
+    double step = 0.1;
+    while(precision--){
+        double j = square;
+        while (j*j<=n)
+        {
+            square = j;
+            j+=step;
+
+        }
+        step/=10;
+        
+    }
+    return square;
+}
+
 int main()
 {
+    //TC: O(logn) + O(precision)
     int n;
-    cout << "enter the input " << endl;
+    cout << "enter the input : " << endl;
     cin >> n;
-    int square = sqrt(n);
-    cout << "this is the out put of given value " << n << " and this is square root " << endl;
+    double square = precisionSQRT(n);
+
+    //cout<<ans<<endl; //cout prints only upto 5 precision
+    cout << "this is the out put of given value " << n << " and this is square root : "<<square << endl;
+
+    //to print precision more than 5 use c printer:
+
+    printf("Precision sqrt:  %.12f\n", square);
 
     return 0;
 }

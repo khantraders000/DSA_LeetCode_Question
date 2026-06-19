5c9228587abafc02b3556455edafdf9976676fc2
Ayan Khan <ayan180013@gmail.com>
2026-06-19 23:07:20 +0530

    logic of finding number is even or odd

---
+++ EvenOddCheckingByInputValue.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int EvenOrOdd(int n)
+{
+    if (n % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "give the input" << endl;
+    cin >> n;
+    int ans = EvenOrOdd(n);
+    if (ans)
+    {
+        cout << "the input value is even: " << n << endl;
+    }
+    else
+    {
+        cout << "the input value is odd: " << n << endl;
+    }
+
+    return 0;
+}

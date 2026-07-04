8103e4e0b8a589358840288fbcad36f7aa3f2421
Ayan khan <210683560+khantraders000@users.noreply.github.com>
2026-07-04 21:55:58 +0530

    Update binary_search-in-2d-array.cpp

--- binary_search-in-2d-array.cpp
+++ binary_search-in-2d-array.cpp
@@ -28,6 +28,8 @@ bool binarySearch(int arr[][4], int rows, int cols, int target)
     return false;
 }
 
+//commit 
+
 int main()
 {
     int arr[5][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}};
